5d69b22fd6c09d215128e0644472bfcd32481620
Abe Binder <abrahbin@amazon.com>
2021-09-03 22:24:07 -0700

    counts words

--- src/adverbs.py
+++ src/adverbs.py
@@ -8,7 +8,12 @@ if __name__ == '__main__':
     f = open('../data/test_clean.txt')
     raw = f.read()
     tokens = nltk.word_tokenize(raw)
-    pos = nltk.pos_tag(tokens)
+    pos_tags = nltk.pos_tag(tokens)
+    dictionary = {}
+    for pos in pos_tags:
+        #RB is abbrevaition for adverb
+       if(pos[1] == 'RB'):
+           word = pos[0]
+           dictionary[word] = dictionary.get(word, 0) + 1
 
-
-    print(pos)
+    print(dictionary)
