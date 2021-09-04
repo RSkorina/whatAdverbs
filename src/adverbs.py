eaedd16a24bbc3f15a0121088bbff4119d07eb20
Abe Binder <abrahbin@amazon.com>
2021-09-03 22:45:19 -0700

    prettified

--- src/adverbs.py
+++ src/adverbs.py
@@ -1,8 +1,10 @@
 import nltk
 from collections import defaultdict
 
+# requirements for nltk
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
+
 
 def tokenize_file(file_path):
     file = open(file_path)
@@ -10,32 +12,31 @@ def tokenize_file(file_path):
     words = nltk.word_tokenize(raw_text)
     return nltk.pos_tag(words)
 
+
 def count_adverbs(words_with_pos_tags):
     adverb_instances = defaultdict(int)
-    adverbCounter = 0
+    adverb_counter = 0
     for pos in words_with_pos_tags:
-        #RB is abbrevaition for adverb
-        if(pos[1] == 'RB'):
+        # RB is abbrevaition for adverb
+        if (pos[1] == 'RB'):
             word = pos[0]
-            adverb_instances[word] +=1
-            adverbCounter+=1
-    return adverb_instances, adverbCounter
+            adverb_instances[word] += 1
+            adverb_counter += 1
+    return adverb_instances, adverb_counter
+
+
+def print_results(adverb_instances, adverb_counter, words_with_pos_tags):
+    for word in sorted(adverb_instances, key=adverb_instances.get, reverse=True):
+        print(word, adverb_instances[word])
+    adverb_percentage = adverb_counter / len(words_with_pos_tags) * 100
+    print("adverb percentage is %f" % adverb_percentage)
 
 
 def analyze(file_path):
     words_with_pos_tags = tokenize_file(file_path)
-    adverb_instances = defaultdict(int)
-    adverbCounter = 0
-    for pos in words_with_pos_tags:
-        #RB is abbrevaition for adverb
-        if(pos[1] == 'RB'):
-            word = pos[0]
-            adverb_instances[word] +=1
-            adverbCounter+=1
-    for w in sorted(adverb_instances, key=adverb_instances.get, reverse=True):
-        print(w, adverb_instances[w])
-    adverb_percentage = adverbCounter/len(words_with_pos_tags) * 100
-    print("adverb percentage is %f"%adverb_percentage)
+    results = count_adverbs(words_with_pos_tags)
+    print_results(*results, words_with_pos_tags)
+
 
 if __name__ == '__main__':
     analyze('../data/test_clean.txt')
