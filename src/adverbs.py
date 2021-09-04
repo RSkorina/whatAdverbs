cc15f82b77832887dfdd4cd8721aaec62c3d6aab
Abe Binder <abrahbin@amazon.com>
2021-09-03 22:28:15 -0700

    prints

--- src/adverbs.py
+++ src/adverbs.py
@@ -1,4 +1,6 @@
 import nltk
+from collections import defaultdict
+
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
@@ -9,11 +11,11 @@ if __name__ == '__main__':
     raw = f.read()
     tokens = nltk.word_tokenize(raw)
     pos_tags = nltk.pos_tag(tokens)
-    dictionary = {}
+    dictionary = defaultdict(int)
     for pos in pos_tags:
         #RB is abbrevaition for adverb
        if(pos[1] == 'RB'):
            word = pos[0]
-           dictionary[word] = dictionary.get(word, 0) + 1
-
-    print(dictionary)
+           dictionary[word] +=1
+    for w in sorted(dictionary, key=dictionary.get, reverse=True):
+        print(w, dictionary[w])
