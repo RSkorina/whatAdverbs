52c3c0a51f4ebcf298211369a08e487eac9915ca
Abe Binder <abebinder@gmail.com>
2021-09-06 14:11:15 -0700

    added word count

--- src/sentiment.py
+++ src/sentiment.py
@@ -5,11 +5,13 @@ import re
 
 from nltk.sentiment import SentimentIntensityAnalyzer
 nltk.download('vader_lexicon')
+
+word_tokenizer = nltk.RegexpTokenizer(r'\w+')
 sia = SentimentIntensityAnalyzer()
 
 def analyze(input_directory, output_directory):
     listdir = os.listdir(input_directory)
-    for dir in ["shortStories.txt"]:
+    for dir in ["test_sentiment.txt"]:
         print(dir)
         output_file_name = os.path.splitext(dir)[0] + '.csv'
         input_full_path = '%s/%s' % (input_directory, dir)
@@ -23,10 +25,17 @@ def single_analyze(input_file_path, output_file_path):
     setences = nltk.sent_tokenize(raw_text)
     arr = []
     for setence in setences:
-        arr.append(sia.polarity_scores(setence))
+        count = len(word_tokenizer.tokenize(setence))
+        scores = sia.polarity_scores(setence)
+        scores['word_count'] = count
+        arr.append(scores)
     frame = pd.DataFrame(arr)
     frame.to_csv(output_file_path)
     print(frame)
+
+def count_words(text):
+    tokens = word_tokenizer.tokenize(text)
+    print(tokens)
 
 if __name__ == '__main__':
     analyze("../data/raw_text", "../data/sentiment")
