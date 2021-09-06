4677b9fd8864d8efe4544d5dcfa30b79e1c7c5de
Abe Binder <abebinder@gmail.com>
2021-09-06 14:13:58 -0700

    changed some variable names

--- src/sentiment.py
+++ src/sentiment.py
@@ -11,12 +11,11 @@ sia = SentimentIntensityAnalyzer()
 
 def analyze(input_directory, output_directory):
     listdir = os.listdir(input_directory)
-    for dir in ["test_sentiment.txt"]:
-        print(dir)
-        output_file_name = os.path.splitext(dir)[0] + '.csv'
-        input_full_path = '%s/%s' % (input_directory, dir)
-        output_full_path = "%s/%s" %(output_directory, output_file_name)
-        single_analyze(input_full_path,output_full_path)
+    for file_name in ["test_sentiment.txt"]:
+        input_file_full_path = '%s/%s' % (input_directory, file_name)
+        output_file_name = os.path.splitext(file_name)[0] + '.csv'
+        output_file_full_path = "%s/%s" %(output_directory, output_file_name)
+        single_analyze(input_file_full_path,output_file_full_path)
 
 #TODO add rows in sentence
 def single_analyze(input_file_path, output_file_path):
